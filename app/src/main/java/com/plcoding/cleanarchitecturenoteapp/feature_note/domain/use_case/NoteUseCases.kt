370545f798f688916b934dd941f4f90ac0890d02
HBazai <bazaee.hassan@gmail.com>
2023-07-06 12:54:37 +0330

    Ready for UI

--- app/src/main/java/com/plcoding/cleanarchitecturenoteapp/feature_note/domain/use_case/NoteUseCases.kt
+++ app/src/main/java/com/plcoding/cleanarchitecturenoteapp/feature_note/domain/use_case/NoteUseCases.kt
@@ -3,5 +3,6 @@ package com.plcoding.cleanarchitecturenoteapp.feature_note.domain.use_case
 // Step 9
 data class NoteUseCases(
     val getNotes: GetNotes,
-    val deleteNote: DeleteNote
+    val deleteNote: DeleteNote,
+    val addNote: AddNote
 )
--- app/src/main/java/com/plcoding/cleanarchitecturenoteapp/feature_note/presentation/notes/NoteViewModel.kt
+++ app/src/main/java/com/plcoding/cleanarchitecturenoteapp/feature_note/presentation/notes/NoteViewModel.kt
@@ -6,7 +6,13 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.plcoding.cleanarchitecturenoteapp.feature_note.domain.model.Note
 import com.plcoding.cleanarchitecturenoteapp.feature_note.domain.use_case.NoteUseCases
+import com.plcoding.cleanarchitecturenoteapp.feature_note.domain.util.NoteOrder
+import com.plcoding.cleanarchitecturenoteapp.feature_note.domain.util.OrderType
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -18,13 +24,21 @@ class NoteViewModel @Inject constructor(
 
     private val _state = mutableStateOf(NotesState())
     val state: State<NotesState> = _state
-
+    private var getNotesJob: Job? = null
     private var recentlyDeletedNote: Note? = null
 
+    init {
+        getNotes(NoteOrder.Date(OrderType.Descending))
+    }
     fun onEvent(event: NotesEvent) {
         when (event) {
             is NotesEvent.Order -> {
-
+                if (state.value.noteOrder::class == event.noteOrder::class &&
+                    state.value.noteOrder.orderType == event.noteOrder.orderType
+                ) {
+                    return
+                }
+                getNotes(event.noteOrder)
             }
 
             is NotesEvent.DeleteNote -> {
@@ -36,7 +50,8 @@ class NoteViewModel @Inject constructor(
 
             is NotesEvent.RestoreNote -> {
                 viewModelScope.launch {
-
+                    noteUseCases.addNote(recentlyDeletedNote ?: return@launch)
+                    recentlyDeletedNote = null
                 }
             }
 
@@ -46,5 +61,14 @@ class NoteViewModel @Inject constructor(
                 )
             }
         }
+    }
+
+    private fun getNotes(noteOrder: NoteOrder) {
+        getNotesJob?.cancel()
+        getNotesJob = noteUseCases.getNotes(noteOrder)
+            .onEach { notes ->
+                _state.value = state.value.copy(notes = notes, noteOrder = noteOrder)
+            }
+            .launchIn(viewModelScope)
     }
 }
